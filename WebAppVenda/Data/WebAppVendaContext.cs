88a65866200c16a78bf699e67be0540629b5961b
Wanderson Costa <msn.wanderson@hotmail.com>
2021-01-15 07:49:01 -0300

    Criando a estrutura CRUD de departamentos

---
+++ WebAppVenda/Data/WebAppVendaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppVenda.Models;
+
+namespace WebAppVenda.Data
+{
+    public class WebAppVendaContext : DbContext
+    {
+        public WebAppVendaContext (DbContextOptions<WebAppVendaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppVenda.Models.Department> Department { get; set; }
+    }
+}
